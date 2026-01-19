e53bbb266178da4a2e0f2275461af3158653c803
Jagan <jagansudan04@gmail.com>
2026-01-19 17:25:37 +1300

    link to testflight

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const APP_SCHEME = process.env.NEXT_PUBLIC_APP_SCHEME || 'lfg';
-export const APP_STORE_URL = process.env.NEXT_PUBLIC_APP_STORE_URL || 'https://apps.apple.com/us/app/letsfitgobeta/id6754862826';
+export const APP_STORE_URL = process.env.NEXT_PUBLIC_APP_STORE_URL || 'https://testflight.apple.com/join/2YD8sVE4';
 export const PLAY_STORE_URL = process.env.NEXT_PUBLIC_PLAY_STORE_URL;
 
 
